b7cef2bccb7370852a50c1798518304d930758dc
Irlene Galiza <irleneviturino@gmail.com>
2021-07-15 18:09:00 +0100

    Applying CSS style

--- alurakut/pages/_app.js
+++ alurakut/pages/_app.js
@@ -1,10 +1,21 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
+
 const GlobalStyle = createGlobalStyle`
+  /* Reset o CSS */
+  {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: sans-serife;
+    font-size: 20px;
+    background-color: #D9E6F6;   
   }
 `
 
