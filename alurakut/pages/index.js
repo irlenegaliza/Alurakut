11e2058ca6dbb0c8d9d16e024b0f110ff03990a6
Irlene Galiza <irleneviturino@gmail.com>
2021-07-16 17:58:59 +0100

    Increading the MainGrid

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,4 +1,6 @@
-import styled from 'styled-components'
+
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
 
 /* const Title = styled.h1`
   font-size: 50px;
@@ -8,25 +10,29 @@ import styled from 'styled-components'
 // Inicial maiúscula porque se trata de um componente e não de uma <tag>
 // A "div" se trata da tag usada para criar o quadrado 
 // Tudo o que está entre crases será o CSS 
-const Box = styled.div `
-  background: #ffffff;
-  border-radius: 8px;
-`;
- 
+
+
 export default function Home() {
   // Tudo o que é escrito no return será renderizado
   // Usa-se o "()" dentro do return  para obter a quebra de linha
   return (
-    <main>
-      <Box>
-        Imagem
+    <MainGrid>
+      {/* Chave: valor */}
+      <div>
+      <Box className="profileArea" style = {{ gridArea: 'profileArea'}}>
+        <img src="https://github.com/irlenegaliza.png" />
       </Box>
-      <Box>
+      </div>
+      <div>
+      <Box className="welcomeArea" style= {{gridArea: 'welcomeArea'}}>
         Bem-vindo
       </Box>
-      <Box>
+      </div>
+      <div>
+      <Box className="profileRelationsArea" style = {{ gridArea: 'profileRelationsArea'}}> 
         Comunidades
         </Box>
-    </main>
+        </div>
+    </MainGrid>
   )
 }
